71c29ac42bcda08e1628b815d9873349dce3d209
NeLeGaL <nelegal@nelegal-VirtualBox.(none)>
2013-03-10 00:03:55 +0300

    quiet wget

--- weather.sh
+++ weather.sh
@@ -1,3 +1,3 @@
-wget meteoprog.by/ru/weather/minsk
-
-grep 'property="og:description" content=".*"' minsk | grep -o 'content=".*"'| grep -o '".*"' | grep -o '[^"]*'
+#!/bin/sh
+wget --quiet meteoprog.by/ru/weather/minsk 
+grep 'property="og:description" content=".*"' minsk| grep -o 'content=".*"'| grep -o '".*"' | grep -o '[^"]*'
