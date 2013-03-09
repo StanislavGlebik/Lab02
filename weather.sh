052882ac8e2e773d35bc7dc5bdc42216e54b6066
NeLeGaL <nelegal@nelegal-VirtualBox.(none)>
2013-03-10 01:09:06 +0300

    Added ini-files

--- weather.sh
+++ weather.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-wget --quiet meteoprog.by/ru/weather/minsk  -O -|grep 'property="og:description" content=".*"' | grep -o 'content=".*"'| grep -o '".*"' | grep -o '[^"]*'
+
+interval=`grep -o 'interval[ ]*=[ ]*[0-9]*\.\?[0-9]*' weather.ini | grep -o '[0-9]*\.\?[0-9]*'`
+echo $interval
+while true
+do
+	wget --quiet meteoprog.by/ru/weather/minsk  -O -|grep -o 'property="og:description" content=".*"' | grep -o 'content=".*"'| grep -o '".*"' | grep -o '[^"]*'
+	sleep $interval
+done
