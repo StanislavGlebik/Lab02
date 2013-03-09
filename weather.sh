9c7316faf6256f6384a985a6f8560d4e13730a5e
NeLeGaL <nelegal@nelegal-VirtualBox.(none)>
2013-03-09 23:21:02 +0300

    Stupid grep (actually, a lot of grep, because didn't find how to use groups in grep)

--- weather.sh
+++ weather.sh
@@ -1,3 +1,3 @@
 wget meteoprog.by/ru/weather/minsk
 
-grep 'property=og:description" content="\(.*\"' minsk
+grep 'property="og:description" content=".*"' minsk | grep -o 'content=".*"'| grep -o '".*"' | grep -o '[^"]*'
